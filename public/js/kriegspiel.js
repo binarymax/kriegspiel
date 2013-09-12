3ef8604dc4f37980633f85f40358fc3c9fb705c5
Max Irwin <max_irwin@yahoo.com>
2013-09-12 10:34:55 +0100

    Movestate persisted

--- public/js/kriegspiel.js
+++ public/js/kriegspiel.js
@@ -1,17 +1,55 @@
+//Hello
 var kriegspiel = (function() {
 
-	var _variant = {'occupies':true}; 
+	var _variant = {'occupies':true};
 	var _colors  = {'w':'white','b':'black'};
 	var _colorx  = {'white':'black','black':'white'};
 	var _socket  = io.connect('http://'+document.domain);
 	var _gameid  = location.href.substr(location.href.indexOf('/games/')+7);
-	var _active  = false;
+
+	var _active  = false; //Your turn is active or not
 	
 	var _color; //Your color
 	var _oppos; //Your opponents color
 	var _board; //Your board
 	var _p,_o;  //First chars of your color and opponents color
 	var _temp;  //Temporary position to reset illegal moves
+
+	//Finite State Machine for the options available to the user on the move
+	var _movestate = (function(){
+		var movestate = function(){this.reset()};
+			movestate.prototype = {
+				okOccupies:function(){ return this.occupies<0; },
+				isOccupies:function(){ return this.occupies===0; },
+				doOccupies:function(){ return ++this.occupies; },
+				okDrawOffer:function(){ return this.drawoffer<0; },
+				isDrawOffer:function(){ return this.drawoffer===0; },				
+				doDrawOffer:function(){ return ++this.drawoffer; },
+				okPawnCaptures:function(){ return this.pawncaptures<0; },
+				isPawnCaptures:function(){ return this.pawncaptures===0; },
+				doPawnCaptures:function(){ return ++this.pawncaptures; },
+				reset : function(){			
+					this.occupies=-1;
+					this.pawncaptures=-1;
+					this.drawoffer=-1;
+				},
+				serialize:function(){
+					var self=this;
+					return {
+						occupies:self.occupies,
+						pawncaptures:self.pawncaptures,
+						drawoffer:self.drawoffer
+					};
+				},
+				deserialize:function(states){
+					var self=this;
+					self.occupies=states.occupies;
+					self.pawncaptures=states.pawncaptures;
+					self.drawoffer=states.drawoffer;
+				}
+			};
+		return new movestate();
+	})();
 
 	//Common board functions:
 	var ranks = '1,2,3,4,5,6,7,8'.split(',');
@@ -25,6 +63,10 @@ var kriegspiel = (function() {
 		_oppos = _colorx[color];
 		_p = _color.charAt(0);
 		_o = _oppos.charAt(0);
+	}
+	
+	//Resets the movestate	
+	var freshmove = function() {
 	}
 	
 	//Announces a message to the player
@@ -48,6 +90,7 @@ var kriegspiel = (function() {
 
 	//Activates player's ability to move
 	var activate = function() {
+		if(!_active) _movestate.reset(); 
 		_active=true;
 	}
 	
@@ -100,11 +143,21 @@ var kriegspiel = (function() {
 		//Set up the pieces
 		_board.position(pos,false);
 	}
+	
+	//Loads the movestate from the server, and disables actions if necessary
+	var loadmovestate = function(state) {
+		_movestate.deserialize(state);
+		/*
+		if(!_movestate.okPawnCaptures()) { $("#pawncaptures").disable(); }
+		if(!_movestate.okDrawOffer()) { $("#offerdraw").disable(); }
+		if(!_movestate.okOccupies()) { $("#occupies").disable(); }
+		*/
+	}
 		
 	//-----------------------------------------
 	//Socket Events
 	var onMove = function (data) {
-		announce(data);
+		announce(data); 
 		if (data.action === 'start') activate();
 		else deactivate();
 	};
@@ -171,12 +224,13 @@ var kriegspiel = (function() {
 		
 		var turn = 'white';
 		if (data.game) {
-			loadposition(data.game.position,data.game.scratch);
-			loadmessages(data.game.messages||[]);
 			turn = _colors[data.game.turn||'w'];
+			loadposition(data.game.position,data.game.scratch);
+			loadmessages(data.game.messages||[]);			
 		}
 
 		if (_color===turn) activate();
+		if (data.game) loadmovestate(data.game[turn+'state']);
 		
 	};
 
@@ -214,18 +268,27 @@ var kriegspiel = (function() {
 
 	
 	var doPawncaptures = function(e){
-		if(_active) _socket.emit('pawncaptures',{gameid:_gameid});	
+		console.log(_movestate.okPawnCaptures());
+		if(_active && _movestate.okPawnCaptures()) {
+			_socket.emit('pawncaptures',{gameid:_gameid});
+			_movestate.doPawnCaptures();
+		}	
 		return nobubble(e);
 	};
 
 
-	var clearOccupies = function() { for(var f=0;f<8;f++) for(var r=0;r<8;r++) $('.square-'+files[f]+ranks[r]).removeClass('highlight-square').removeClass('highlight-occupies'); }
+	var clearOccupies = function() {
+		for(var f=0;f<8;f++) { 
+			for(var r=0;r<8;r++) { 
+				$('.square-'+files[f]+ranks[r]).removeClass('highlight-square').removeClass('highlight-occupies');
+			}
+		} 
+	}
 
 	var doOccupiesSquare = function(e) {
 		clearOccupies();
 		if(_active && _variant.occupies) {
 			var square = $(this).attr("data-square");
-			console.log(square);
 			_socket.emit('occupies',{gameid:_gameid,target:square});
 		}
 		return nobubble(e);
@@ -233,50 +296,27 @@ var kriegspiel = (function() {
 
 	var doOccupies = function(e){
 		clearOccupies();
-		if(_active && _variant.occupies) {
+		if(_active && _variant.occupies && _movestate.okOccupies()) {
+			_movestate.doOccupies();
+			var f,r;
 			var position = _board.position();
 			var hasPiece = function(sq) { return (position.hasOwnProperty(sq) && position[sq].charAt(0)===_p)?true:false; }
-			var f,fm,fl,fr; //file middle,left,right
-			var r,rm,ra,rb; //rank middle,above,below
-			var adj = {};
+			var addAdj   = function(fx,ry) { 
+				if(fx && ry && !hasPiece(fx+ry)) {
+					$('.square-'+ fx+ry).attr("data-square",fx+ry).addClass('highlight-square').addClass('highlight-occupies'); 
+				}
+			};
 			for(var sq in position) {
 				if (hasPiece(sq)) {
 					f = getFile(sq.charAt(0));
-					fm = files[f];
-					fl = f>0?files[f-1]:'';
-					fr = f<7?files[f+1]:'';
-
 					r = getRank(sq.charAt(1));
-					rm = ranks[r];
-					ra = r>0?ranks[r-1]:'';
-					rb = r<7?ranks[r+1]:'';
-
-					if(fl) {
-						if(ra && !hasPiece(fl+ra)) adj[fl+ra]=1;
-						if(rm && !hasPiece(fl+rm)) adj[fl+rm]=1;
-						if(rb && !hasPiece(fl+rb)) adj[fl+rb]=1;
+					for(var x=-1;x<=1;x++) {
+						for(var y=-1;y<=1;y++) {
+							if(x||y) addAdj(files[f+x],ranks[r+y]);			
+						}
 					}
-					
-					if(fm) {
-						if(ra && !hasPiece(fm+ra)) adj[fm+ra]=1;
-						if(rm && !hasPiece(fm+rm)) adj[fm+rm]=1;
-						if(rb && !hasPiece(fm+rb)) adj[fm+rb]=1;
-					}
-					
-					if(fr) {
-						if(ra && !hasPiece(fr+ra)) adj[fr+ra]=1;
-						if(rm && !hasPiece(fr+rm)) adj[fr+rm]=1;
-						if(rb && !hasPiece(fr+rb)) adj[fr+rb]=1;
-					}
-
 				}
 			}
-			for(var sq in adj) {
-				if(adj.hasOwnProperty(sq) && adj[sq]===1) {
-					$('.square-'+ sq).attr("data-square",sq).addClass('highlight-square').addClass('highlight-occupies');
-				}
-			}
-
 		}
 		return nobubble(e);
 	};
